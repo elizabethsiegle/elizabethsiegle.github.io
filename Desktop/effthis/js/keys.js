3ec36e86b77b29646d32fe4a89bbf3305c65b139
Elizabeth Siegle <lizzie.siegle@gmail.com>
2016-10-28 00:25:26 -0400

    weijfweijf

---
+++ Desktop/effthis/js/keys.js
@@ -0,0 +1,13 @@
+// keys.js
+// Contains keys to run this demo
+// The keys should be kept secret and separate from the code
+// Enter your keys below to take the first in running this step 
+// VROOOOM VROOM
+
+// Returns an object with the required credentials
+function getKeys () {
+	return {
+		CLARIFAI_CLIENT_ID: "tAlEF7EHsEfwJSNdFcF-irGsE1cHDExxHuvt7O68",
+		CLARIFAI_CLIENT_SECRET: "qfX4CeN02wePHqFym0s_nzHc1tEZUV5rlwwAQOUx"
+	}
+}
