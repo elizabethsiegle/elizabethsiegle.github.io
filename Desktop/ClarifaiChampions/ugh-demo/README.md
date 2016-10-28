9bb842317fce49009bcde40cbb2acb4b103043ed
Elizabeth Siegle <lizzie.siegle@gmail.com>
2016-10-28 00:12:15 -0400

    ugh

---
+++ Desktop/ClarifaiChampions/ugh-demo/README.md
@@ -0,0 +1,1 @@
+Basic web demo using Clarifai API. 
---
+++ Desktop/ClarifaiChampions/ugh-demo/js/keys.js
@@ -0,0 +1,13 @@
+// keys.js
+// Contains keys to run this demo
+// The keys should be kept secret and separate from the code
+// Enter your keys below to take the first in running this step 
+// VROOOOM VROOM
+
+// Returns an object with the required credentials
+function getKeys () {
+	return {
+		CLARIFAI_CLIENT_ID: "VHXFGbkAVSk7Xp4Ul4gx17OOmlaayGQvUOh0VQH0",
+		CLARIFAI_CLIENT_SECRET: "EYy15ptmeyd8dKDgUgU0Fe3Tr-HixVGYdwzGv7DL"
+	}
+}
