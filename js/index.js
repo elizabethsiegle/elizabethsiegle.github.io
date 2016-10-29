6c89f79825c37c2b7264936e007625a400ff19fe
Elizabeth Siegle <lizzie.siegle@gmail.com>
2016-10-29 16:42:34 -0400

    first commit, ty will strimling from twitter and kevin

---
+++ js/index.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+  $('.about, .talksworkshops, .coffeetalk, .press').css({'visibility':'hidden'});
+
+  $(window).scroll(function() { 
+    var height = $(window).scrollTop();
+
+    if (height > 190) {
+      $('.about').css({"visibility": "visible"}).addClass('animated fadeIn');
+      $('.press').css({"visibility": "visible"}).addClass('animated fadeIn');
+    }
+
+    if (height > 1190) { 
+      $('.talksworkshops').css({"visibility": "visible"}).addClass('animated fadeIn');
+    }
+
+    
+      $('.coffeetalk').css({"visibility": "visible"}).addClass('animated fadeIn');
+
+  });
+
+  /* Main button clicks */
+  $('body').on('click', '#about', function() {
+    $('html, body').animate({
+          scrollTop: $(".about").offset().top
+    }, 500);
+    return false;
+  });
+
+  $('body').on('click', '#coffeetalk', function() {
+    $('html, body').animate({
+        scrollTop: $(".coffeetalk").offset().top
+    }, 500);
+    return false;
+  });
+
+  $('body').on('click', '#talksworkshops', function() {
+    $('html, body').animate({
+        scrollTop: $(".talksworkshops").offset().top
+    }, 500);
+    return false;
+  });
+
+
+});
