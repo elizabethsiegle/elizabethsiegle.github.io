015637634b1303d98868dac0a1dabfca73a48fd8
Elizabeth Siegle <lizzie.siegle@gmail.com>
2017-03-12 14:33:18 -0400

    lol fix js, resume name, css, idk

--- js/custom.js
+++ js/custom.js
@@ -1,14 +1,14 @@
 function init() {
-    window.addEventListener('scroll', function(e) {
-        var distanceY = window.pageYOffset || document.documentElement.scrollTop, shrinkOn = 750, header = document.querySelector(".header");
-        if (distanceY > shrinkOn) {
-            classie.add(header, "smaller");
-        } else {
-            if (classie.has(header, "smaller")) {
-                classie.remove(header, "smaller");
-            }
-        }
-    });
+    // window.addEventListener('scroll', function(e) {
+    //     var distanceY = window.pageYOffset || document.documentElement.scrollTop, shrinkOn = 750, header = document.querySelector(".header");
+    //     if (distanceY > shrinkOn) {
+    //         classie.add(header, "smaller");
+    //     } else {
+    //         if (classie.has(header, "smaller")) {
+    //             classie.remove(header, "smaller");
+    //         }
+    //     }
+    // });
 }
 window.onload = init();
 var TxtType = function(el, toRotate, period) {
