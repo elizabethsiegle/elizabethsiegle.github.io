20d491ce62c7ace99cf799453a3163efaa16c30b
Lizzie Siegle <lizziedev24@gmail.com>
2020-07-02 17:40:43 -0700

    twitch link, updated talk links

--- js/custom.js
+++ js/custom.js
@@ -1,16 +1,3 @@
-function init() {
-    window.addEventListener('scroll', function(e) {
-        var distanceY = window.pageYOffset || document.documentElement.scrollTop, shrinkOn = 750, header = document.querySelector(".header");
-        if (distanceY > shrinkOn) {
-            classie.add(header, "smaller");
-        } else {
-            if (classie.has(header, "smaller")) {
-                classie.remove(header, "smaller");
-            }
-        }
-    });
-}
-window.onload = init();
 var TxtType = function(el, toRotate, period) {
     this.toRotate = toRotate;
     this.el = el;
