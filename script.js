ab97fd0db7c36bb5456b63b45583e2297f09c494
Lizzie Siegle <esiegle@brynmawr.edu>
2021-01-08 02:18:45 -0800

    lmao folders, rename css, toggle extras to be minimalist

--- script.js
+++
@@ -1,8 +0,0 @@
-// Select the button
-const btn = document.querySelector('.btn-toggle');
-
-// Listen for a click on the button
-btn.addEventListener('click', function() {
-  // Then toggle (add/remove) the .dark-theme class to the body
-  document.body.classList.toggle('dark-theme');  
-})
