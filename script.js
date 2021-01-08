6b43173f2b3abac45434f3033600fa610ee95b9b
Lizzie Siegle <esiegle@brynmawr.edu>
2021-01-07 21:52:59 -0800

    dark mode plus links

---
+++ script.js
@@ -0,0 +1,8 @@
+// Select the button
+const btn = document.querySelector('.btn-toggle');
+
+// Listen for a click on the button
+btn.addEventListener('click', function() {
+  // Then toggle (add/remove) the .dark-theme class to the body
+  document.body.classList.toggle('dark-theme');  
+})
