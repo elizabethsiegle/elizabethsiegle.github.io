b3480da5f6dd2f9987b12e15ccf83865c204e2e8
Lizzie Siegle <esiegle@brynmawr.edu>
2021-01-13 21:58:47 -0800

    idk why another error in js, i bloody hate css

--- js/custom.js
+++ js/custom.js
@@ -61,16 +61,16 @@ window.onload = function() {
     document.body.appendChild(css);
 };
 
-$(function() {
-    $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html,body').animate({
-                    scrollTop: target.offset().top
-                }, 2500);
-            }
-        }
-    });
-});
+// $(function() {
+//     $('a[href*=#]:not([href=#])').click(function() {
+//         if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+//             var target = $(this.hash);
+//             target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+//             if (target.length) {
+//                 $('html,body').animate({
+//                     scrollTop: target.offset().top
+//                 }, 2500);
+//             }
+//         }
+//     });
+// });
